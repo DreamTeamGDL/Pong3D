9b9543430c4f419e4661dc72ff9ef081cdacc0ff
Miguel Pérez García <miguelpg_95@hotmail.com>
2018-11-05 14:50:53 -0600

    Fixing not defined function

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -1,5 +1,10 @@
 import GameRequester from "./services/GameRequester";
 
+document.addEventListener("DOMContentLoaded", () => {
+    (document.getElementById("button") as HTMLButtonElement).onclick = main;
+    console.log("ready");
+});
+
 function main(){
     let username = (document.getElementById("nicknameTxt") as HTMLInputElement).value;
     let requester = new GameRequester();
