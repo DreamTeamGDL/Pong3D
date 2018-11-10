799582576a9374d85dc4ffe597534dfb45aaef66
eldelcesar <cesarjr.espinosa@gmail.com>
2018-11-09 22:02:14 -0600

    Solved conflicts pt 2

--- frontend/src/app.ts
+++ frontend/src/app.ts
@@ -2,32 +2,17 @@ import GameRequester from "./services/GameRequester";
 import Scene from "./webgl/scene";
 
 document.addEventListener("DOMContentLoaded", () => {
-<<<<<<< HEAD
     (document.getElementById("startButton") as HTMLButtonElement).addEventListener("click", () => main());
 });
 
 async function main(){
     const scene = new Scene("canvas", false);
     scene.launch();
-=======
-	(document.getElementById("button") as HTMLButtonElement).onclick = main;
-	console.log("ready");
-});
-
-function main(){
-	setTimeout(() => {
-		console.log("Hello");
-	}, 500);
->>>>>>> 6da5202d7a7fd695921b13daea5825383843425f
-
     let username = (document.getElementById("nicknameTxt") as HTMLInputElement).value;
     let requester = new GameRequester();
     requester.start(username);
-<<<<<<< HEAD
 
     setTimeout(() => {
         console.log("Hello");
     }, 5000000);
-=======
->>>>>>> 6da5202d7a7fd695921b13daea5825383843425f
 }
